fd94cfffdf5e1619da134e663bf503fb1d1658cf
AmrAbdulSalam <amrsalam@hotmail.com>
2023-07-24 12:09:09 +0300

    Init program

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryManagementSystem.ProgramInfo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+        }
+    }
+}
