47b12dea28a4d6bb4b51161014158304b46f47d7
unknown <amrsalam@hotmail.com>
2023-10-24 16:43:22 +0300

    Update the 'InsertToDatabase' function

--- Connection.cs
+++ Connection.cs
@@ -6,7 +6,7 @@ namespace InventorySystem
 {
     class Connection
     {
-        private string _connectionString = "Data Source=DESKTOP-NUK8KNC\\SQLEXPRESS;Initial Catalog=Inventory;Integrated Security=True";
+        private readonly string _connectionString = "Data Source=DESKTOP-NUK8KNC\\SQLEXPRESS;Initial Catalog=Inventory;Integrated Security=True";
         public void InsertToDatabase(Product product)
         {
             var query = $"INSERT INTO Products VALUES ('{product.ProductName}' , {product.ProductPrice} , {product.ProductQuantity})";
@@ -16,32 +16,6 @@ namespace InventorySystem
                 var cmd = new SqlCommand(query, con);
                 cmd.ExecuteNonQuery();
             }
-        }
-
-        public async Task<string> ViewProductsAsync()
-        {
-            using (var con = new SqlConnection(_connectionString))
-            {
-                await con.OpenAsync();
-                var query = "SELECT * FROM Products";
-                var output = "Product Name \t Product Price \t Product Quant\n";
-                using (var cmd = new SqlCommand(query, con))
-                {
-                    using (var reader = await cmd.ExecuteReaderAsync())
-                    {
-                        while (await reader.ReadAsync())
-                        {
-                            output +=
-                            reader.GetValue(0).ToString() + "\t" +
-                            reader.GetValue(1).ToString() + "\t" +
-                            reader.GetValue(2).ToString() + "\n";
-                        }
-                        return output;
-                    }
-                }
-       
-            }
-
         }
     }
 }
