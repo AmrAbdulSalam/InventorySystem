b7f571598145d1259fcf20eb45db48780d38905f
AmrAbdulSalam <amrsalam@hotmail.com>
2023-10-25 14:25:21 +0300

    Updating 'InsertProduct' function

--- Connection.cs
+++ Connection.cs
@@ -9,18 +9,11 @@ namespace InventorySystem
         private static readonly string _connection = "mongodb+srv://amr:IapNpXMCOT535J8E@cluster0.mwp6uhf.mongodb.net/Inventory?retryWrites=true&w=majority";
         private static readonly MongoClient _client = new MongoClient(_connection);
         private static readonly IMongoDatabase _database = _client.GetDatabase("Inventory");
-        private static readonly IMongoCollection<BsonDocument> _collection = _database.GetCollection<BsonDocument>("Products");
+        private static readonly IMongoCollection<Product> _collection = _database.GetCollection<Product>("Products");
 
         public static void InsertProduct(Product product)
         {
-            var document = new BsonDocument
-            {
-                {"Product Name" , product.ProductName },
-                {"Product Price" , product.ProductPrice },
-                {"Product Quantitiy" , product.ProductQuantity },
-            };
-
-            _collection.InsertOne(document);
+            _collection.InsertOne(product);
         }
 
     }
